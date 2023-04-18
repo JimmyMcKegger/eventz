c0cfe1af2d62bd7e8e9e01116fe427436023cd17
James <jamesrobertsongroves@gmail.com>
2023-04-19 00:08:55 +0100

    create new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,18 +9,31 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event)
+  end
+
   def edit
     @event = Event.find(params[:id])
   end
 
   def update
     @event = Event.find(params[:id])
-    event_params = 
-      params.require(:event).
-      permit(:name, :description, :location, :price, :starts_at)
 
     @event.update(event_params)
 
     redirect_to event_path(@event)
+  end
+
+  private
+  def event_params
+    params.require(:event).
+      permit(:name, :description, :location, :price, :starts_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
 
   root "events#index"
 
-  get 'events' => 'events#index'
-  get 'events/:id' => 'events#show', as: "event" # this sets an event_path for the link_to() helper method
-  get 'events/:id/edit' => 'events#edit', as: "edit_event"
-  patch 'events/:id' => "events#update"
+  resources :events
 end
