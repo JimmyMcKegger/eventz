37b3559abbc53d9529cf06579038a03200362761
James <jamesrobertsongroves@gmail.com>
2023-04-18 22:36:03 +0100

    event form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
 
   root "events#index"
-  
+
   get 'events' => 'events#index'
   get 'events/:id' => 'events#show', as: "event" # this sets an event_path for the link_to() helper method
+  get 'events/:id/edit' => 'events#edit', as: "edit_event"
 end
