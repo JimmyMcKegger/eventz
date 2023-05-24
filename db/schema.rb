1d2c050bf955da4a1276451e5c4286b7bf9b5896
James <jamesrobertsongroves@gmail.com>
2023-05-25 00:41:53 +0100

    rubocopped

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,28 +12,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_232308) do
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "starts_at"
-    t.text "description"
-    t.string "image_file_name", default: "placeholder.png"
-    t.integer "capacity", default: 1
+ActiveRecord::Schema[7.0].define(version: 20_230_524_232_308) do
+  create_table 'events', force: :cascade do |t|
+    t.string 'name'
+    t.string 'location'
+    t.decimal 'price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.datetime 'starts_at'
+    t.text 'description'
+    t.string 'image_file_name', default: 'placeholder.png'
+    t.integer 'capacity', default: 1
   end
 
-  create_table "registrations", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "how_heard"
-    t.integer "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_registrations_on_event_id"
+  create_table 'registrations', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'how_heard'
+    t.integer 'event_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['event_id'], name: 'index_registrations_on_event_id'
   end
 
-  add_foreign_key "registrations", "events"
+  add_foreign_key 'registrations', 'events'
 end
