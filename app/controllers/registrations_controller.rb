1673f303dc33869ff0435ea18fecd1fb8630a39b
James <jamesrobertsongroves@gmail.com>
2023-08-20 14:30:26 +0100

    one-to-many forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,45 @@
 # frozen_string_literal: true
 
 class RegistrationsController < ApplicationController
+  before_action :set_event
+
+  def index
+    @registrations = @event.registrations.all
+  end
+
+  def new
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @registration = @event.registrations.new(registration_params)
+
+    if @registration.save
+      redirect_to event_registrations_url(@event),
+                  notice: 'Thanks for registering'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @registration = @event.registrations.find(params[:id])
+  end
+
+  def destroy
+    @registration = @event.registrations.find(params[:id])
+    @registration.destroy
+
+    redirect_to events_url, status: :see_other, notice: 'Registration successfully deleted!'
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def registration_params
+    params.require(:registration).permit(:name, :email, :how_heard)
+  end
 end
