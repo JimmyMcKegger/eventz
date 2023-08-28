e88ec46435326514750d000d83c99b537e5f7ae1
James <jamesrobertsongroves@gmail.com>
2023-08-28 23:44:02 +0100

    registrations on user page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class RegistrationsController < ApplicationController
+  before_action :require_signin
   before_action :set_event
+
 
   def index
     @registrations = @event.registrations.all
@@ -13,7 +15,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @event.registrations.new(registration_params)
-
+    @registration.user = current_user
     if @registration.save
       redirect_to event_registrations_url(@event),
                   notice: 'Thanks for registering'
@@ -40,6 +42,6 @@ class RegistrationsController < ApplicationController
   end
 
   def registration_params
-    params.require(:registration).permit(:name, :email, :how_heard)
+    params.require(:registration).permit(:how_heard)
   end
 end
