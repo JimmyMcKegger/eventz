9eb8f604ad416fac0aa2f12ae749afe381a93898
James <jamesrobertsongroves@gmail.com>
2023-08-28 09:10:37 +0100

    user-registrations models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,18 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_url, alert: 'Please sign in!'
   end
 
+  def current_user_admin?
+    current_user&.admin?
+  end
+
+  helper_method :current_user_admin?
+
+  def require_admin
+    return if current_user_admin?
+
+    redirect_to events_url, alert: 'Unauthorized access!'
+  end
+
   def current_user?(user)
     current_user == user
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_820_233_228) do
+ActiveRecord::Schema[7.0].define(version: 20_230_828_075_606) do
   create_table 'events', force: :cascade do |t|
     t.string 'name'
     t.string 'location'
@@ -26,12 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 20_230_820_233_228) do
   end
 
   create_table 'registrations', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
     t.string 'how_heard'
     t.integer 'event_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.integer 'user_id'
     t.index ['event_id'], name: 'index_registrations_on_event_id'
   end
 
@@ -41,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_820_233_228) do
     t.string 'password_digest'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'admin', default: false
   end
 
   add_foreign_key 'registrations', 'events'
