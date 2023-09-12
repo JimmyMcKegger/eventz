a089ded8521832e72abbbce6f5ede3ed8ab20136
James <jamesrobertsongroves@gmail.com>
2023-09-12 08:54:32 +0100

    image blobs

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,13 +15,14 @@ class Event < ApplicationRecord
   has_many :categorizations, dependent: :destroy
   has_many :categories, through: :categorizations
 
+  has_one_attached :main_image
+
   # Validations
   validates :name, presence: true, uniqueness: true
   validates :location, presence: true
   validates :description, presence: true, length: { minimum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
-  validates :image_file_name, format: { with: /\w+\.(jpg|png)\z/i, message: 'Must be a valid JPG or PNG image' }
 
   # Scopes
   scope :past, -> { where('starts_at < ?', Time.now).order('starts_at') }
